2d9599e70ac7144c8a4271a8dd60ae7ef5785441
Sammy Hajhamid <sh@pepsi.pw>
2024-07-14 18:16:35 -0400

    asdas

--- lib/plugins/index.ts
+++ lib/plugins/index.ts
@@ -1,5 +1,10 @@
+export abstract class ResourceBlockConfig<Schema> {
+  public abstract key: string;
+  public abstract schema: Zod.Schema<Schema>;
+}
+
 export abstract class Plugin {
-  public abstract readonly key: string;
+  public abstract readonly name: string;
 
   public abstract getChallengeConfigSchema(): any;
 }
