3d853aa03bfa5938df81db87656c9d816c7f4f6d
enyoukai <an.joseph0001@gmail.com>
2024-07-15 19:35:30 -0700

    changing factory to method

--- lib/challenges/loader/factory.ts
+++ lib/challenges/loader/factory.ts
@@ -2,13 +2,11 @@ import type { Loader, LoaderConfig } from ".";
 import { ClassicFileLoader } from "./classic";
 import { ResourceFileLoader } from "./resource";
 
-export class LoaderFactory {
-	static generateLoader(loaderConfig: LoaderConfig): any { // running into some issues with getting it to return generics right now
-		if (loaderConfig.loaderType === "classic") return new ClassicFileLoader(loaderConfig);
-		else if (loaderConfig.loaderType === "resource") return new ResourceFileLoader(loaderConfig);
-		else 
-		{
-			throw new Error("LoaderFactory called with unknown loaderType");
-		}
+export function generateLoader(loaderConfig: LoaderConfig): any { // running into some issues with getting it to return generics right now
+	if (loaderConfig.loaderType === "classic") return new ClassicFileLoader(loaderConfig);
+	else if (loaderConfig.loaderType === "resource") return new ResourceFileLoader(loaderConfig);
+	else 
+	{
+		throw new Error("LoaderFactory called with unknown loaderType");
 	}
 }
