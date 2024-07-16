afcca9d7be5da3273a1184bf6e07d42cae297b24
Sammy Hajhamid <sh@pepsi.pw>
2024-07-16 05:46:45 -0400

    sutff

--- lib/challenges/loader/resource/resource.ts
+++ lib/challenges/loader/resource/resource.ts
@@ -8,35 +8,30 @@ export class Resource {
     if (typeof definition === "string" && definition.includes("${")) {
       const regex = /\${(.*?)}/g;
       const matches = definition.match(regex);
-      if (matches) {
-        matches.forEach((match) => {
-          const dependency = match.replace("${", "").replace("}", "");
-          this.dependencies.push(new Dependency("plugin", dependency));
-        });
-      }
     }
+    return definition;
   }
 
   findDependencies(definition: AnyObject): any {
     if (Array.isArray(definition)) {
       return definition.map((item) => this.findDependencies(item));
     } else if (definition instanceof Object) {
-      return Object.keys(definition).map((key) =>
-        this.findDependencies(definition[key]),
+      return Object.fromEntries(
+        Object.keys(definition).map((key) => [
+          key,
+          this.findDependencies(definition[key]),
+        ])
       );
     } else {
-      this.createPossibleDependency(definition);
+      return this.createPossibleDependency(definition);
     }
   }
 
   constructor(plugin: any, definition: Record<string, any>) {
-    this.findDependencies(definition);
+    console.log(this.findDependencies(definition));
   }
 }
 
 class Dependency {
-  constructor(
-    public id: string,
-    public name: string,
-  ) {}
+  constructor(public id: string, public name: string) {}
 }
