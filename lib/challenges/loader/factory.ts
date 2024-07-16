aefc54ecbbfe2dc09737f1e70b90591c23e47d12
Sammy Hajhamid <sh@pepsi.pw>
2024-07-15 22:42:33 -0400

    fix

--- lib/challenges/loader/factory.ts
+++ lib/challenges/loader/factory.ts
@@ -1,13 +1,14 @@
-import type { LoaderConfig } from ".";
+import type { Loader, LoaderConfig } from ".";
 import { ClassicFileLoader } from "./classic";
 import { ResourceFileLoader } from "./resource";
 
 // fixme: fix return type
-export function createLoader(loaderConfig: LoaderConfig): any { 
-	if (loaderConfig.loaderType === "classic") return new ClassicFileLoader(loaderConfig);
-	else if (loaderConfig.loaderType === "resource") return new ResourceFileLoader(loaderConfig);
-	else 
-	{
-		throw new Error("LoaderFactory called with unknown loaderType");
-	}
+export function createLoader(loaderConfig: LoaderConfig): Loader<LoaderConfig> {
+  if (loaderConfig.loaderType === "classic")
+    return new ClassicFileLoader(loaderConfig);
+  else if (loaderConfig.loaderType === "resource")
+    return new ResourceFileLoader(loaderConfig);
+  else {
+    throw new Error("LoaderFactory called with unknown loaderType");
+  }
 }
