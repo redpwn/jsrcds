53292e83ffaf1bdefb2c58353a7fceac0866aea5
Sammy Hajhamid <sh@pepsi.pw>
2024-08-05 16:09:18 -0400

    stuff

--- packages/loaders/classic/challenge.ts
+++ packages/loaders/classic/challenge.ts
@@ -2,4 +2,13 @@ import type { ChallengeConfig } from "./schema";
 
 export class Challenge {
   constructor(public config: ChallengeConfig) {}
+
+  createResources() {
+    // create container resources: build container
+    // create cluster resources: Namespaces, Deployments, Services, NetworkPolicies, and TraefikRoutes
+    // -- potentially klodd resources!
+    // create bucket resources: upload files
+    // create scoreboard resources: push challenge, file urls
+    // monitoring, adminbot?
+  }
 }
